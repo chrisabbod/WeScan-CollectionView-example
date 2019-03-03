3276ae3570ffd0073238fb354d0e0f3f31366c75
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2019-03-03 16:29:03 -0500

    Add document scanning functionality

--- WeScanCollectionViewExample/Controller/DocumentCollectionVC.swift
+++ WeScanCollectionViewExample/Controller/DocumentCollectionVC.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import WeScan
 import RealmSwift
 
-class DocumentCollectionVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class DocumentCollectionVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, ImageScannerControllerDelegate {
 
     private let reuseIdentifier = "customDocumentCell"
     @IBOutlet weak var documentCollectionView: UICollectionView!
@@ -38,7 +39,43 @@ class DocumentCollectionVC: UIViewController, UICollectionViewDataSource, UIColl
         return cell
     }
     
+    //MARK: - WeScan Methods
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
     @IBAction func addDocumentButton(_ sender: UIBarButtonItem) {
+        let scannerViewController = ImageScannerController()
+        scannerViewController.imageScannerDelegate = self
+        present(scannerViewController, animated: true)
+    }
+    
+    func imageScannerController(_ scanner: ImageScannerController, didFailWithError error: Error) {
+        // You are responsible for carefully handling the error
+        print(error)
+    }
+    
+    func imageScannerController(_ scanner: ImageScannerController, didFinishScanningWithResults results: ImageScannerResults) {
         
+        let newDocument = Document()
+        
+        //create image url
+        let imageName: String = newDocument.uuid
+        let imagePath: String = "\(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])/\(imageName).png"
+        let imageUrl: URL = URL(fileURLWithPath: imagePath)
+        
+        //store image url
+        let newImage: UIImage = results.scannedImage
+        try? newImage.pngData()?.write(to: imageUrl) //Write the pngData to a location
+        
+        newDocument.imagePath = imagePath
+        
+        //saveDocument(newDocument)
+        
+        scanner.dismiss(animated: true)
+    }
+    
+    func imageScannerControllerDidCancel(_ scanner: ImageScannerController) {
+        // The user tapped 'Cancel' on the scanner
+        // You are responsible for dismissing the ImageScannerController
+        scanner.dismiss(animated: true)
     }
 }
